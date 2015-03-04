4553211fe23486ec7527b10cd38dab84a94fcd1b
Jordan <jordan.aranda@me.com>
2015-03-04 21:44:39 +0100

    Primer cmake

---
+++ debug/CMakeFiles/Robot.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Robot/Dagu/Dagu.cc" "/home/pi/Robot/debug/CMakeFiles/Robot.dir/Dagu/Dagu.cc.o"
+  "/home/pi/Robot/QTRSensors/QTRSensors.cc" "/home/pi/Robot/debug/CMakeFiles/Robot.dir/QTRSensors/QTRSensors.cc.o"
+  "/home/pi/Robot/Robot.cc" "/home/pi/Robot/debug/CMakeFiles/Robot.dir/Robot.cc.o"
+  "/home/pi/Robot/SPI/SPIUtils.cc" "/home/pi/Robot/debug/CMakeFiles/Robot.dir/SPI/SPIUtils.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
